f31e5fe78bf5eac3866d1436d80e2d0836bc200d
Angela Caldas <aes.caldas@gmail.com>
2022-07-15 17:27:04 -0300

    wip: sidebar content

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -5,11 +5,11 @@ import '../styles/components/sidebar.sass';
 const Sidebar = () => {
   return (
     <aside id='sidebar'>
-      <p>imagem</p>
+      <img src={Avatar} alt="Angela Caldas" />
       <p className="title">Desenvolvedor</p>
       <p>Redes sociais</p>
       <p>Informações de contato</p>
-      <a href="" className="btn">Download</a>
+      <a href="" className="btn">Download currículo</a>
     </aside>
   );
 };
