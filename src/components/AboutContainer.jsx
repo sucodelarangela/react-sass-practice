fb4c6e5b234188e577bd14fd8a48d4b047d8ac79
Angela Caldas <aes.caldas@gmail.com>
2022-07-21 20:19:13 -0300

    about container component

---
+++ src/components/AboutContainer.jsx
@@ -0,0 +1,12 @@
+
+const AboutContainer = () => {
+  return (
+    <section className="about-container">
+      <h2>Sobre</h2>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nisi illum tenetur explicabo, expedita praesentium sint porro ullam quasi ipsum id exercitationem non voluptas unde repudiandae quia illo adipisci aliquid nam?</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, perferendis. A rerum maiores inventore modi vitae beatae. Consequatur, minus aut in quo nihil voluptates ratione architecto alias consectetur nobis reprehenderit?</p>
+    </section>
+  );
+};
+
+export default AboutContainer;
