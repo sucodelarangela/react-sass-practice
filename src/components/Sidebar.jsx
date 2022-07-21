c54079af8c6d5bc5a5501e9c74f5b6a183817e15
Angela Caldas <aes.caldas@gmail.com>
2022-07-21 17:04:33 -0300

    sidebar component finished

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,5 @@
 import SocialNetworks from './SocialNetworks';
+import InformationContainer from './InformationContainer';
 
 import Avatar from '../img/angela.jpeg';
 
@@ -10,7 +11,7 @@ const Sidebar = () => {
       <img src={Avatar} alt="Angela Caldas" />
       <p className="title">Desenvolvedora Front End</p>
       <SocialNetworks />
-      <p>Informações de contato</p>
+      <InformationContainer />
       <a href="" className="btn">Download currículo</a>
     </aside>
   );
